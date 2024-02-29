34ba4507d47e452804c0b1a01e46a49bd563f4be
markoka101 <markoka101@gmail.com>
2024-02-28 20:46:01 -0500

    added animation for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 
 import {BadgeCheckIcon, ChipIcon} from "@heroicons/react/solid";
 import {languages, frameworks, database, tools} from "../data";
@@ -15,6 +16,98 @@ export default function Skills() {
   const databaseName = showData ? 'Hide Database & Industry Tools':'Show Database & Industry Tools';
   const toolName = showTools ? 'Hide Tools/Software':'Show Tools/Software';
 
+  //display programming languages
+  const displayLanguage =         
+  <Transition 
+  show={showLang}
+  enter="transition-all duration-975 overflow-hidden"
+          enterFrom="transform scale-95 opacity-0 max-h-0"
+          enterTo="transform scale-100 opacity-100 duration-700"
+          leave="transition duration-475 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+  className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 mx-2">
+    {languages.map((languages) => (
+      <div key={languages} className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+          <span className="title-font font-medium text-white">
+            {languages}
+          </span>
+        </div>
+      </div>
+      ))}
+  </Transition>
+
+  //display framworks and libraries
+  const displayFrameworks =
+  <Transition 
+  show={showFrame}
+  enter="transition-all duration-975 overflow-hidden"
+          enterFrom="transform scale-95 opacity-0 max-h-0"
+          enterTo="transform scale-100 opacity-100 duration-700"
+          leave="transition duration-475 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+  className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 mx-2">
+    {frameworks.map((frameworks) => (
+      <div key={frameworks} className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+          <span className="title-font font-medium text-white">
+            {frameworks}
+          </span>
+        </div>
+      </div>
+    ))}
+  </Transition>
+
+  //display database and industry tools
+  const displayDatabase = 
+  <Transition 
+  show={showData}
+  enter="transition-all duration-975 overflow-hidden"
+          enterFrom="transform scale-95 opacity-0 max-h-0"
+          enterTo="transform scale-100 opacity-100 duration-700"
+          leave="transition duration-475 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+  className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 mx-2">
+    {database.map((database) => (
+      <div key={database} className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+          <span className="title-font font-medium text-white">
+            {database}
+          </span>
+        </div>
+      </div>
+    ))}
+  </Transition>
+
+  //display Tools/Software
+  const displayTools = 
+  <Transition
+  show={showTools}
+  enter="transition-all duration-975 overflow-hidden"
+          enterFrom="transform scale-95 opacity-0 max-h-0"
+          enterTo="transform scale-100 opacity-100 duration-700"
+          leave="transition duration-475 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+  className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 mx-2">
+    {tools.map((tools) => (
+      <div key={tools} className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+          <span className="title-font font-medium text-white">
+            {tools}
+          </span>
+        </div>
+      </div>
+    ))}
+  </Transition>
+
     return (
       <section id="skills">
         <div className="container px-5 py-10 mx-auto">
@@ -27,102 +120,45 @@ export default function Skills() {
 
           <div className="container px-5 py-1 mx-auto">
             <div className="text-center items-center justify-center">
-              <button className="inline-flex text-slate-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
+              <button className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" 
+              onClick={()=> {
                 setShowLang(!showLang)
-              }}><h2>{languageName}</h2></button>
-              {showLang && displayLanguage}
+              }}>
+                <h2>{languageName}</h2>
+                </button>
+              {displayLanguage}
             </div>
           </div>
 
 
           <div className="container px-5 py-1 mx-auto">
             <div className="text-center">  
-              <button className="inline-flex text-slate-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
+              <button className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
                 setShowFrame(!showFrame)
               }}><h2>{frameworkName}</h2></button>
-              {showFrame && displayFrameworks}
+              {displayFrameworks}
             </div>
           </div>
 
 
           <div className="container px-5 py-1 mx-auto">
             <div className="text-center">
-              <button className="inline-flex text-slate-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
+              <button className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
                 setShowData(!showData)
               }}><h2>{databaseName}</h2></button>
-              {showData && displayDatabase}
+              {displayDatabase}
             </div>
           </div>
 
-          <div className="container px-5 py-1 mx-auto">
+          <div className="container px-5 pt-1 pb-10 mx-auto">
             <div className="text-center">
-              <button className="inline-flex text-slate-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
+              <button className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out" onClick={()=> {
                 setShowTools(!showTools)
               }}><h2>{toolName}</h2></button>
-              {showTools && displayTools}
+              {displayTools}
             </div>
           </div>
-          </div>
+        </div>
     </section>
     );
 }
-
-//display programming languages
-const displayLanguage =         
-  <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-    {languages.map((languages) => (
-      <div key={languages} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-          <span className="title-font font-medium text-white">
-            {languages}
-          </span>
-        </div>
-      </div>
-      ))}
-  </div>
-
-//display framworks and libraries
-const displayFrameworks =
-  <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-    {frameworks.map((frameworks) => (
-      <div key={frameworks} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-          <span className="title-font font-medium text-white">
-            {frameworks}
-          </span>
-        </div>
-      </div>
-    ))}
-  </div>
-
-  //display database and industry tools
-  const displayDatabase = 
-  <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-    {database.map((database) => (
-      <div key={database} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-          <span className="title-font font-medium text-white">
-            {database}
-          </span>
-        </div>
-      </div>
-    ))}
-  </div>
-
-  //display Tools/Software
-  const displayTools = 
-  <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-    {tools.map((tools) => (
-      <div key={tools} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-          <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-          <span className="title-font font-medium text-white">
-            {tools}
-          </span>
-        </div>
-      </div>
-    ))}
-  </div>
