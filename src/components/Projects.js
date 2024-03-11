4a2d4793e4948b7142a0f3bfba3c94cc6a2dc02a
markoka101 <markoka101@gmail.com>
2024-03-11 18:53:32 -0400

    removed useless code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
           <div className="container px-5 py-10 mx-auto text-center lg:px-40">
             <div className="flex flex-col w-full mb-20">
               <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-              <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+              <h1 className="sm:text-4xl text-3xl font-medium mb-4 text-white">
                 My Projects
               </h1>
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-300">
@@ -26,10 +26,10 @@ export default function Projects() {
                   className="w-1/2  min-h-full">
                   <div className="m-2">
                     <div className="flex flex-col px-8 py-10 m-0 w-full h-64 border-4 border-gray-800 bg-gray-900 hover:bg-slate-900 hover:border-slate-400 items-center justify-center rounded-xl transition-all duration-300 ease-in-out">
-                      <h2 className="hidden md:block lg:block tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                      <h2 className="hidden md:block lg:block tracking-widest text-sm font-medium text-green-400 mb-1">
                         {project.subtitle}
                       </h2>
-                      <h1 className="title-font text-lg font-medium text-white mb-3">
+                      <h1 className="text-lg font-medium text-white mb-3">
                         {project.title}
                       </h1>
                       <p className="leading-relaxed">{project.description}</p>
