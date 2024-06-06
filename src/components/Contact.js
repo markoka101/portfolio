6e3c73435ed53ff75f688c4d9d1879cc05a5f802
markoka101 <markoka101@gmail.com>
2024-06-06 18:27:01 -0400

    removed background colors from components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,103 +1,115 @@
 import React from "react";
 
 export default function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
 
-  const [ name, setName ] = React.useState('');
-  const [ email, setEmail ] = React.useState('');
-  const [ message, setMessage ] = React.useState('');
-
-  function encode(data) {
-    return Object.keys(data).map((key) => 
-      encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&');
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (name === '' || email === '' || message === '') {
-      alert('All fields must be filled');
-      return;
+    function encode(data) {
+        return Object.keys(data)
+            .map(
+                (key) =>
+                    encodeURIComponent(key) +
+                    "=" +
+                    encodeURIComponent(data[key])
+            )
+            .join("&");
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    fetch('/' , {
-      method: 'POST',
-      headers: { "Content-Type": 'application/x-www-form-urlencoded'},
-      body: encode({ "form-name": 'contact', name, email, message })
-    })
-    .then(() => {
-      alert('Message Sent');
-    })
-    .catch(() => {
-      alert('Something Went Wrong');
-    })
-  }
+        if (name === "" || email === "" || message === "") {
+            alert("All fields must be filled");
+            return;
+        }
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, email, message }),
+        })
+            .then(() => {
+                alert("Message Sent");
+            })
+            .catch(() => {
+                alert("Something Went Wrong");
+            });
+    }
 
     return (
         <section id="contact" className="relative">
-          <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-            <form
-              netlify
-              name="contact"
-              onSubmit={handleSubmit}
-              className="lg:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0 p-5 border-slate-500 border-4 bg-gray-900 rounded-xl">
-              <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                Contact Me
-              </h2>
-              <p className="text-white leading-relaxed mb-5">
-                Please leave your name, email, and a short message
-              </p>
-              <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="message"
-                  className="leading-7 text-sm text-gray-400">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  onChange={(e) => {
-                    setMessage(e.target.value);
-                  }}
-                />
-              </div>
-              <button
-                type="submit"
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-xl text-lg transition-colors translate-y-1 duration-300 ease-in-out">
-                Submit
-              </button>
-            </form>
-          </div>
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <form
+                    netlify
+                    name="contact"
+                    onSubmit={handleSubmit}
+                    className="lg:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0 p-5 border-slate-500 border-4 bg-gray-900 rounded-xl"
+                >
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Contact Me
+                    </h2>
+                    <p className="text-white leading-relaxed mb-5">
+                        Please leave your name, email, and a short message
+                    </p>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="name"
+                            className="leading-7 text-sm text-gray-400"
+                        >
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="email"
+                            className="leading-7 text-sm text-gray-400"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400"
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-xl text-lg transition-colors translate-y-1 duration-300 ease-in-out"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </section>
-      );
+    );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,161 +5,23 @@ import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import { languages, frameworks, database, tools } from "../data";
 
 export default function Skills() {
-  const [showLang, setShowLang] = useState(false);
-  const [showFrame, setShowFrame] = useState(false);
-  const [showData, setShowData] = useState(false);
-  const [showTools, setShowTools] = useState(false);
+    const [showLang, setShowLang] = useState(false);
+    const [showFrame, setShowFrame] = useState(false);
+    const [showData, setShowData] = useState(false);
+    const [showTools, setShowTools] = useState(false);
 
-  const languageName = showLang
-    ? "Hide Programming Languages"
-    : "Show Programming Languages";
-  const frameworkName = showFrame
-    ? "Hide Frameworks & Libraries"
-    : "Show Frameworks & Libraries";
-  const databaseName = showData
-    ? "Hide Database & Industry Tools"
-    : "Show Database & Industry Tools";
-  const toolName = showTools
-    ? "Hide Tools & Software"
-    : "Show Tools & Software";
+    const languageName = showLang
+        ? "Hide Programming Languages"
+        : "Show Programming Languages";
+    const frameworkName = showFrame
+        ? "Hide Frameworks & Libraries"
+        : "Show Frameworks & Libraries";
+    const databaseName = showData
+        ? "Hide Database & Industry Tools"
+        : "Show Database & Industry Tools";
+    const toolName = showTools
+        ? "Hide Tools & Software"
+        : "Show Tools & Software";
 
-  //display programming languages
-  const displayLanguage = (
-    <Transition
-      show={showLang}
-      enter="transition-all duration-700 overflow-hidden ease transform"
-      enterFrom="opacity-50 max-h-0"
-      enterTo="  opacity-100 max-h-screen"
-      leave="transition-all duration-300 overflow-hidden ease transform"
-      leaveFrom="opacity-100 max-h-screen"
-      leaveTo="opacity-50 max-h-0"
-      className="flex flex-wrap items-center justify-center sm:justify-normal md:justify-normal lg:justify-normal lg:w-3/4 sm:mx-auto sm:mb-2 mx-2"
-    >
-      {languages.map((languages) => (
-        <div key={languages} className="py-2 px-1 sm:w-1/2 w-3/4">
-          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-            <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-            <span className="font-medium text-white">{languages}</span>
-          </div>
-        </div>
-      ))}
-    </Transition>
-  );
-
-  //display framworks and libraries
-  const displayFrameworks = (
-    <Transition
-      show={showFrame}
-      enter="transition-all duration-700 overflow-hidden ease transform"
-      enterFrom="opacity-50 max-h-0"
-      enterTo="  opacity-100 max-h-screen"
-      leave="transition-all duration-300 overflow-hidden ease transform"
-      leaveFrom="opacity-100 max-h-screen"
-      leaveTo="opacity-50 max-h-0"
-      className="flex flex-wrap items-center justify-center sm:justify-normal md:justify-normal lg:justify-normal lg:w-3/4 sm:mx-auto sm:mb-2 mx-2"
-    >
-      {frameworks.map((frameworks) => (
-        <div key={frameworks} className="py-2 px-1 sm:w-1/2 w-3/4">
-          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-            <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-            <span className="font-medium text-white">{frameworks}</span>
-          </div>
-        </div>
-      ))}
-    </Transition>
-  );
-
-  //display database and industry tools
-
-  //display Tools/Software
-  const displayTools = (
-    <Transition
-      show={showTools}
-      enter="transition-all duration-700 overflow-hidden ease transform"
-      enterFrom="opacity-50 max-h-0"
-      enterTo="  opacity-100 max-h-screen"
-      leave="transition-all duration-300 overflow-hidden ease transform"
-      leaveFrom="opacity-100 max-h-screen"
-      leaveTo="opacity-50 max-h-0"
-      className="flex flex-wrap items-center justify-center sm:justify-normal md:justify-normal lg:justify-normal lg:w-3/4 sm:mx-auto sm:mb-2 mx-2"
-    >
-      {tools.map((tools) => (
-        <div key={tools} className="py-2 px-1 sm:w-1/2 w-3/4 ">
-          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-            <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-            <span className="font-medium text-white">{tools}</span>
-          </div>
-        </div>
-      ))}
-    </Transition>
-  );
-
-  return (
-    <section id="skills">
-      <div className="container px-5 py-10 mx-auto">
-        <div className="text-center mb-10">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
-          </h1>
-        </div>
-
-        <div className="container px-5 py-1 mx-auto">
-          <div className="text-center items-center justify-center">
-            <button
-              className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 mb-2 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out"
-              onClick={() => {
-                setShowLang(!showLang);
-              }}
-            >
-              <h2>{languageName}</h2>
-            </button>
-            {displayLanguage}
-          </div>
-        </div>
-
-        <div className="container px-5 py-1 mx-auto">
-          <div className="text-center">
-            <button
-              className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 mb-2 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out"
-              onClick={() => {
-                setShowFrame(!showFrame);
-              }}
-            >
-              <h2>{frameworkName}</h2>
-            </button>
-            {displayFrameworks}
-          </div>
-        </div>
-
-        <div className="container px-5 py-1 mx-auto">
-          <div className="text-center">
-            <button
-              className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 mb-2 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out"
-              onClick={() => {
-                setShowData(!showData);
-              }}
-            >
-              <h2>{databaseName}</h2>
-            </button>
-            {displayDatabase}
-          </div>
-        </div>
-
-        <div className="container px-5 pt-1 pb-10 mx-auto">
-          <div className="text-center">
-            <button
-              className="inline-flex text-gray-50 justify-center bg-green-500 border-0 py-2 px-6 mb-2 focus:outline-none hover:bg-emerald-600 hover:text-white rounded text-lg w-3/4 transition-colors duration-300 ease-in-out"
-              onClick={() => {
-                setShowTools(!showTools);
-              }}
-            >
-              <h2>{toolName}</h2>
-            </button>
-            {displayTools}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return <section id="skills"></section>;
 }
