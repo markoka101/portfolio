2450d1e48606002764d3ea92b88fd34a42fe6e0c
markoka101 <markoka101@gmail.com>
2024-06-12 17:10:28 -0400

    oops

--- src/data.js
+++ src/data.js
@@ -7,32 +7,24 @@ export const projects = [
         subtitle: "Javascript | JSX | React | CSS | Tailwind",
         description: "Portfolio developed for client",
         link: "https://www.taylor-m.com/",
-        position: 0,
-        el: document.getElementById("carousel-item-1"),
     },
     {
         title: "Event Tracker",
         subtitle: "Java | Javascript | Spring Boot | JPA | React | Mapbox",
         description: "Event Tracking application",
         link: "https://github.com/markoka101/event-tracker",
-        position: 1,
-        el: document.getElementById("carousel-item-2"),
     },
     {
         title: "TODO Application",
         subtitle: "Java | Javascript | Spring Boot | JPA | React",
         description: "Application to keep track of tasks",
         link: "https://github.com/markoka101/todo",
-        position: 2,
-        el: document.getElementById("carousel-item-3"),
     },
     {
         title: "Lesson Plan",
         subtitle: "Javascript | Node.js",
         description: "Lesson plan I developed for my students",
         link: "https://github.com/markoka101/lessons",
-        position: 3,
-        el: document.getElementById("carousel-item-4"),
     },
 ];
 
