8aa1a3023e8c92cf9ffe9583fc7e799ec263f06b
markoka101 <markoka101@gmail.com>
2024-06-21 14:12:07 -0400

    added margin root

--- src/components/RevealOnScroll.js
+++ src/components/RevealOnScroll.js
@@ -3,9 +3,12 @@ export default function RevealOnScroll(ref) {
     const [isIntersecting, setIntersecting] = useState(false);
 
     useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            setIntersecting(entry.isIntersecting);
-        });
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIntersecting(entry.isIntersecting);
+            },
+            { rootMargin: "-200px" }
+        );
 
         observer.observe(ref.current);
 
