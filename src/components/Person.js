8b22c7d1411366ff9d59981896507497fe888b5b
markoka101 <markoka101@gmail.com>
2024-07-17 14:21:08 -0400

    added bg and changed y margin

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Person() {
     return (
-        <section id="person" className="">
-            <div className="container mx-auto flex px-05 py-20 md:flex-row flex-col items-center">
+        <section id="person" className="min-h-[92vh] justify-center flex">
+            <div className="container mx-auto flex px-5 py-10 my-20 md:flex-row flex-col items-center  border-4 bg-black bg-opacity-50 rounded-lg justify-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="sm:text-4xl mb-5 font-medium text-white">
                         About Me
