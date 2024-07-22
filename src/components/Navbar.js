64ce57a24e79391fd370e2293e5fb7625b0e4c7b
markoka101 <markoka101@gmail.com>
2024-07-22 15:50:45 -0400

    changed default  styling from flowbite

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,55 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function NavbarComponent() {
     const navigate = useNavigate();
+
+    const customTheme = {
+        root: {
+            base: "!bg-gray-800 !bg-opacity-80 sticky top-0 z-10 px-0 py-2.5 sm:px-4",
+            rounded: {
+                on: "rounded",
+                off: "",
+            },
+            bordered: {
+                on: "border",
+                off: "",
+            },
+            inner: {
+                base: "mx-0 px-2 flex flex-wrap items-center justify-between min-w-full",
+                fluid: {
+                    on: "",
+                    off: "container",
+                },
+            },
+        },
+        brand: {
+            base: "flex items-center",
+        },
+        collapse: {
+            base: "w-full md:block md:w-auto",
+            list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
+            hidden: {
+                on: "hidden",
+                off: "",
+            },
+        },
+        link: {
+            base: "block py-2 pl-3 pr-4 md:p-0",
+            active: {
+                on: "bg-cyan-700 text-white dark:text-white md:bg-transparent md:text-cyan-700",
+                off: "border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white",
+            },
+            disabled: {
+                on: "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
+                off: "",
+            },
+        },
+        toggle: {
+            base: "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden",
+            icon: "h-6 w-6 shrink-0",
+        },
+    };
     return (
-        <Navbar className="!bg-gray-800 !bg-opacity-80 sticky top-0 z-10 justify-between flex min-w-full">
+        <Navbar theme={customTheme}>
             <Navbar.Brand>
                 <button
                     className=" font-medium text-slate-300 hover:text-white mb-1 text-2xl transition-colors duration-300 ease-in-out"
@@ -49,7 +96,7 @@ export default function NavbarComponent() {
                 <div className="flex flex-row justify-end md:my-0 lg:my-0 my-2 mr-2">
                     <a
                         href="https://www.linkedin.com/in/mark-oka/"
-                        className="mx-2 hover:text-white text-slate-300 transition-colors duration-200 ease-in-out flex items-center"
+                        className="mr-2 hover:text-white text-slate-300 transition-colors duration-200 ease-in-out flex items-center"
                     >
                         <FontAwesomeIcon icon={faLinkedin} size="2x" />
                     </a>
