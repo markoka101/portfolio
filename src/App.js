70f0b4aa8b5ceffa13b26f95a11e65187596535b
markoka101 <markoka101@gmail.com>
2024-08-02 15:11:45 -0400

    changed  vh to lvh

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Person from "./components/Person";
 
 export default function App() {
     return (
-        <main className="text-gray-300 bg-[url('../public/subtle-prism.svg')] bg-fixed min-h-screen flex flex-col scroll-smooth">
+        <main className="text-gray-300 bg-[url('../public/subtle-prism.svg')] min-h-[100lvh] flex flex-col  ">
             <BrowserRouter>
                 <Flowbite>
                     <NavbarComponent />
