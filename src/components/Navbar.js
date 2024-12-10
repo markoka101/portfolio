6fc2d278af8d3388f9b99eca24d2417f6214acb4
markoka101 <markoka101@gmail.com>
2024-12-10 16:07:22 -0500

    fixed contact styling when nav collapses

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,8 +84,14 @@ export default function NavbarComponent() {
                     About Me
                 </button>
                 <button
+                    onClick={() => navigate("/testimonials")}
+                    className="btn-nav"
+                >
+                    Testimonials
+                </button>
+                <button
                     onClick={() => navigate("/contact")}
-                    className="md:hidden lg:hiddenbtn-nav flex flex-row justify-end items-center"
+                    className="md:hidden lg:hidden hover:text-white transition-colors duration-200 ease-in-out text-end text-2xl"
                 >
                     Contact Me
                 </button>
