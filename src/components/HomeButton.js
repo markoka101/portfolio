008fa18797b794d215592c237d8f05b7909b582d
markoka101 <markoka101@gmail.com>
2025-05-20 16:43:44 -0400

    seperate component for buttons

---
+++ src/components/HomeButton.js
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function HomeButton({ text, route = ' ', customStyle = ' ' }) {
+	const navigate = useNavigate();
+	return (
+		<button
+			onClick={() => navigate(route)}
+			className={`my-1 inline-flex justify-center rounded-xl border-2 border-transparent px-6 py-2 text-lg transition-all duration-200 ease-in focus:outline-none md:my-0.5 md:mr-2 lg:my-0.5 lg:mr-2 ${customStyle}`}
+		>
+			{`${text}`}
+		</button>
+	);
+}
+
+//validation
+HomeButton.propTypes = {
+	text: PropTypes.string.isRequired,
+	route: PropTypes.string.isRequired,
+	customStyle: PropTypes.string.isRequired
+};
