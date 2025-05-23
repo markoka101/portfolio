5463f277916e49d2e8cb2d4d10d493f517d33490
markoka101 <markoka101@gmail.com>
2025-05-23 15:35:05 -0400

    formatting changes

--- src/data.js
+++ src/data.js
@@ -1,156 +1,156 @@
 import {
-    JavaOriginal,
-    JavascriptOriginal,
-    CplusplusOriginal,
-    SqldeveloperOriginal,
-    Html5Original,
-    Css3Original,
-    SpringOriginal,
-    NodejsOriginal,
-    ExpressOriginal,
-    ReactOriginal,
-    TailwindcssOriginal,
-    GitOriginal,
-    GithubOriginal,
-    HibernateOriginal,
-    PostgresqlOriginal,
-    IntellijOriginal,
-    PostmanOriginal,
-    EclipseOriginal,
-    VscodeOriginal,
-    DockerPlain,
-} from "devicons-react";
+	JavaOriginal,
+	JavascriptOriginal,
+	CplusplusOriginal,
+	SqldeveloperOriginal,
+	Html5Original,
+	Css3Original,
+	SpringOriginal,
+	NodejsOriginal,
+	ExpressOriginal,
+	ReactOriginal,
+	TailwindcssOriginal,
+	GitOriginal,
+	GithubOriginal,
+	HibernateOriginal,
+	PostgresqlOriginal,
+	IntellijOriginal,
+	PostmanOriginal,
+	EclipseOriginal,
+	VscodeOriginal,
+	DockerPlain
+} from 'devicons-react';
 /*
 projects
 */
 export const projects = [
-    {
-        title: "Taylor M Portfolio",
-        subtitle: "Javascript | JSX | React | CSS | Tailwind",
-        description: "Portfolio developed for client",
-        link: "https://www.taylor-m.com/",
-    },
-    {
-        title: "Event Tracker",
-        subtitle: "Java | Javascript | Spring Boot | JPA | React | Mapbox",
-        description: "Event Tracking application",
-        link: "https://github.com/markoka101/event-tracker",
-    },
-    {
-        title: "TODO Application",
-        subtitle: "Java | Javascript | Spring Boot | JPA | React",
-        description: "Application to keep track of tasks",
-        link: "https://github.com/markoka101/todo",
-    },
-    {
-        title: "Lesson Plan",
-        subtitle: "Javascript | Node.js",
-        description: "Lesson plan I developed for my students",
-        link: "https://github.com/markoka101/lessons",
-    },
-    {
-        title: "See More",
-        subtitle: "",
-        description: "See all of my projects on GitHub",
-        link: "https://github.com/markoka101",
-    },
+	{
+		title: 'Taylor M Portfolio',
+		subtitle: 'Javascript | JSX | React | CSS | Tailwind',
+		description: 'Portfolio developed for client',
+		link: 'https://www.taylor-m.com/'
+	},
+	{
+		title: 'Event Tracker',
+		subtitle: 'Java | Javascript | Spring Boot | JPA | React | Mapbox',
+		description: 'Event Tracking application',
+		link: 'https://github.com/markoka101/event-tracker'
+	},
+	{
+		title: 'TODO Application',
+		subtitle: 'Java | Javascript | Spring Boot | JPA | React',
+		description: 'Application to keep track of tasks',
+		link: 'https://github.com/markoka101/todo'
+	},
+	{
+		title: 'Lesson Plan',
+		subtitle: 'Javascript | Node.js',
+		description: 'Lesson plan I developed for my students',
+		link: 'https://github.com/markoka101/lessons'
+	},
+	{
+		title: 'See More',
+		subtitle: '',
+		description: 'See all of my projects on GitHub',
+		link: 'https://github.com/markoka101'
+	}
 ];
 
 /*
 Skills and tools
 */
 export const languages = [
-    {
-        title: "Java",
-        icon: JavaOriginal,
-    },
-    {
-        title: "JavaScript",
-        icon: JavascriptOriginal,
-    },
-    {
-        title: "C++",
-        icon: CplusplusOriginal,
-    },
-    {
-        title: "SQL",
-        icon: SqldeveloperOriginal,
-    },
-    {
-        title: "HTML",
-        icon: Html5Original,
-    },
-    {
-        title: "CSS",
-        icon: Css3Original,
-    },
+	{
+		title: 'Java',
+		icon: JavaOriginal
+	},
+	{
+		title: 'JavaScript',
+		icon: JavascriptOriginal
+	},
+	{
+		title: 'C++',
+		icon: CplusplusOriginal
+	},
+	{
+		title: 'SQL',
+		icon: SqldeveloperOriginal
+	},
+	{
+		title: 'HTML',
+		icon: Html5Original
+	},
+	{
+		title: 'CSS',
+		icon: Css3Original
+	}
 ];
 
 export const frameworks = [
-    {
-        title: "Spring Boot",
-        icon: SpringOriginal,
-    },
-    {
-        title: "Node",
-        icon: NodejsOriginal,
-    },
-    {
-        title: "Express",
-        icon: ExpressOriginal,
-    },
-    {
-        title: "React",
-        icon: ReactOriginal,
-    },
-    {
-        title: "Tailwind",
-        icon: TailwindcssOriginal,
-    },
+	{
+		title: 'Spring Boot',
+		icon: SpringOriginal
+	},
+	{
+		title: 'Node',
+		icon: NodejsOriginal
+	},
+	{
+		title: 'Express',
+		icon: ExpressOriginal
+	},
+	{
+		title: 'React',
+		icon: ReactOriginal
+	},
+	{
+		title: 'Tailwind',
+		icon: TailwindcssOriginal
+	}
 ];
 
 export const database = [
-    {
-        title: "Git",
-        icon: GitOriginal,
-    },
-    {
-        title: "Github",
-        icon: GithubOriginal,
-    },
-    {
-        title: "Hibernate",
-        icon: HibernateOriginal,
-    },
-    {
-        title: "JPA",
-        icon: SqldeveloperOriginal,
-    },
-    {
-        title: "PostgreSQL",
-        icon: PostgresqlOriginal,
-    },
+	{
+		title: 'Git',
+		icon: GitOriginal
+	},
+	{
+		title: 'Github',
+		icon: GithubOriginal
+	},
+	{
+		title: 'Hibernate',
+		icon: HibernateOriginal
+	},
+	{
+		title: 'JPA',
+		icon: SqldeveloperOriginal
+	},
+	{
+		title: 'PostgreSQL',
+		icon: PostgresqlOriginal
+	}
 ];
 
 export const tools = [
-    {
-        title: "IntelliJ",
-        icon: IntellijOriginal,
-    },
-    {
-        title: "Visual Studio",
-        icon: VscodeOriginal,
-    },
-    {
-        title: "Postman",
-        icon: PostmanOriginal,
-    },
-    {
-        title: "Eclipse",
-        icon: EclipseOriginal,
-    },
-    {
-        title: "Docker",
-        icon: DockerPlain,
-    },
+	{
+		title: 'IntelliJ',
+		icon: IntellijOriginal
+	},
+	{
+		title: 'Visual Studio',
+		icon: VscodeOriginal
+	},
+	{
+		title: 'Postman',
+		icon: PostmanOriginal
+	},
+	{
+		title: 'Eclipse',
+		icon: EclipseOriginal
+	},
+	{
+		title: 'Docker',
+		icon: DockerPlain
+	}
 ];
