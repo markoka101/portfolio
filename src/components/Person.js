1d361cfdf010556e2cd3dc65e36c6b2bfa1a0ec7
markoka101 <markoka101@gmail.com>
2025-05-23 15:28:32 -0400

    added fade in

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,7 @@
-import React from 'react';
-
 export default function Person() {
 	return (
-		<section id="person" className="flex justify-center">
-			<div className="container mx-auto mb-12 mt-12 flex min-h-[65vh] w-11/12 flex-col items-center rounded-xl border-4 bg-black bg-opacity-50 px-10 py-10 sm:mt-28 md:mt-28 md:flex-row lg:mt-28">
+		<section id="person" className="animate-fadeIn flex justify-center">
+			<article className="container mx-auto mb-12 mt-12 flex min-h-[65vh] w-11/12 flex-col items-center rounded-xl border-4 bg-black bg-opacity-50 px-10 py-10 sm:mt-28 md:mt-28 md:flex-row lg:mt-28">
 				<div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
 					<h1 className="mb-5 font-medium text-white sm:text-4xl">About Me</h1>
 					<h2 className="sm:text-2x1 mb-1 font-medium text-white">Background</h2>
@@ -25,19 +23,19 @@ export default function Person() {
 						-I am passionate about aviation and as a kid, I wanted to be a pilot
 					</p>
 				</div>
-				<div className="w-5/6 sm:max-w-sm md:w-1/2 lg:w-full">
-					<p className="mb-1 leading-relaxed text-gray-200">
+				<aside className="w-5/6 bg-transparent sm:max-w-sm md:w-1/2 lg:w-full">
+					<h4 className="mb-1 leading-relaxed text-gray-200">
 						Top Left: Hades <br />
 						Bottom Left: Salem <br />
 						Right: Throatcuttah
-					</p>
+					</h4>
 					<img
 						className="rounded-md border-4 border-gray-400 object-cover object-center"
 						alt="gremlins"
 						src="./gremlins.JPG"
 					/>
-				</div>
-			</div>
+				</aside>
+			</article>
 		</section>
 	);
 }
