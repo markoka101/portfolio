301a7293e4ec8d18b53a2c8a5d41f6bc73543432
markoka101 <markoka101@gmail.com>
2025-06-18 10:12:00 -0400

    added twmerge to make overwritting styles easier

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,12 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { twMerge } from 'tailwind-merge';
 
 export default function HomeButton({ text, route = ' ', customStyle = ' ' }) {
 	const navigate = useNavigate();
 	return (
 		<button
 			onClick={() => navigate(route)}
-			className={`my-1 inline-flex justify-center rounded-xl border-2 border-transparent px-6 py-2 text-lg transition-all duration-200 ease-in focus:outline-none md:my-0.5 md:mr-2 lg:my-0.5 lg:mr-2 ${customStyle}`}
+			className={twMerge(
+				`lg:mr-2) my-1 inline-flex justify-center rounded-xl border-2 border-transparent px-6 py-2 text-lg transition-all duration-200 ease-in focus:outline-none md:my-0.5 md:mr-2 lg:my-0.5`,
+				customStyle
+			)}
 		>
 			{`${text}`}
 		</button>
