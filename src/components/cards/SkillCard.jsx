a308625c548e5546669f83c0f65844d3c20ac0ef
markoka101 <markoka101@gmail.com>
2025-07-02 16:16:44 -0400

    organizational changes

---
+++ src/components/cards/SkillCard.jsx
@@ -0,0 +1,9 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import {twMerge} from 'tailwind-merge';
+
+export default function SkillCard({skills, customStyles = ''}) {
+
+}
+
+
