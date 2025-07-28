3cf61b82b9cf426b1dc5c0de297a4ee5a8366eb9
markoka101 <markoka101@gmail.com>
2025-07-28 17:08:15 -0400

    env setup

---
+++ src/vite-env.d.ts
@@ -0,0 +1,41 @@
+/// <reference types="vite/client" />
+// Image type declarations
+declare module '*.jpg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.JPG' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.jpeg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.png' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.gif' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.svg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.webp' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.avif' {
+	const content: string;
+	export default content;
+}
