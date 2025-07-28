933b3f6d6f0e4d546c641b7583ed7173881fb94d
markoka101 <markoka101@gmail.com>
2025-07-28 16:56:29 -0400

    types and constants

---
+++ src/utils/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = [
+	{ name: 'Work', route: '/projects' },
+	{ name: 'Skills', route: '/skills' },
+	{ name: 'About', route: '/about-me' },
+	{ name: 'Testimonials', route: '/testimonials' },
+	{ name: 'Contact', route: '/contact' }
+];
---
+++ src/utils/types/projectsType.ts
@@ -0,0 +1,7 @@
+export interface Project {
+	id: string;
+	title: string;
+	subtitle: string;
+	description: string;
+	link: string;
+}
