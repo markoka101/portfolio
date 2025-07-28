cd131aa653c2af6272895eb6aed42813624202ff
markoka101 <markoka101@gmail.com>
2025-07-28 17:08:42 -0400

    changes for moving to typescript and tailwind v4

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
 	{ ignores: ['dist'] },
+	// JavaScript/JSX block
 	{
 		files: ['**/*.{js,jsx}'],
 		languageOptions: {
@@ -23,8 +25,36 @@ export default [
 		rules: {
 			...js.configs.recommended.rules,
 			...reactHooks.configs.recommended.rules,
-			'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+			'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
 			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+		}
+	},
+	// TypeScript/TSX block
+	{
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: tseslint.parser,
+			ecmaVersion: 2020,
+			sourceType: 'module',
+			globals: globals.browser,
+			parserOptions: {
+				project: './tsconfig.json',
+				ecmaFeatures: { jsx: true }
+			}
+		},
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['error'],
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'react/prop-types': 'off'
 		}
 	}
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import sitemapPlugin from 'vite-plugin-sitemap';
-// https://vite.dev/config/
+
 export default defineConfig({
 	plugins: [
 		react(),
+		tailwindcss(),
 		sitemapPlugin({
 			hostname: 'https://markokaportfolio.com/',
 			dynamicRoutes: ['/', '/projects', '/about-me', '/skills', '/testimonials', '/contact']
