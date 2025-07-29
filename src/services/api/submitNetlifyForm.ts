2359085446369fd49f3ab166d5914cb00d454493
markoka101 <markoka101@gmail.com>
2025-07-28 20:54:42 -0400

    fixed api

--- src/services/api/submitNetlifyForm.ts
+++ src/services/api/submitNetlifyForm.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { encode } from '../utils/netlifyFormEncode';
 
 export async function submitNetlifyForm(form: Record<string, string>) {
-	return axios.post('/', encode({ 'form-name': 'book', ...form }), {
+	return axios.post('/', encode({ 'form-name': 'contact', ...form }), {
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 	});
 }
